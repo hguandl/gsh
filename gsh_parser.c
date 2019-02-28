59d1b7854716ae5897939156ea185af5f2139d30
hguandl <hguandl@gmail.com>
2019-02-28 12:14:02 +0800

    Add more comments

--- gsh_parser.c
+++ gsh_parser.c
@@ -24,6 +24,7 @@ static int escape_quote(char esc, char **p, char **arg_s, char **arg_t) {
     ++(*arg_s);
     ++(*p);
     while (**p != esc) {
+        // No matched quote
         if (**p == '\0')
             return -1;
         ++(*p);
@@ -36,24 +37,29 @@ static int escape_quote(char esc, char **p, char **arg_s, char **arg_t) {
 int parse_args(char *line, char **path_ptr, int *argl, char ***args_ptr) {
     char *s = line;
     char *p = s;
+    // Find the end of exec path
     while (*p != ' ' && *p != '\0')
         ++p;
+    // Setup exec path
     char *path = (char *)malloc((p - s + 1) * sizeof(char));
     strncpy(path, s, p - s);
     path[p - s] = '\0';
 
+    // Initalize argument array with length of 8
     int buf_size = 8;
     *argl = 0;
     char **args = (char **)malloc((buf_size) * sizeof(char *));
-    p = line;
+    p = s;  // Back to the beginning
     while (*p != '\0') {
-        char *arg_s;
-        char *arg_t;
+        char *arg_s;  // start position of current argument
+        char *arg_t;  // end position of current argument
         while (*p == ' ' && *p != '\0')
             ++p;
         arg_s = p;
+        // Handle quotes
         if (*p == '"') {
             if (escape_quote('"', &p, &arg_s, &arg_t)) {
+                // Error, cleanup and return
                 clean_char_arr(*argl, &args);
                 free(path);
                 return -1;
@@ -69,17 +75,18 @@ int parse_args(char *line, char **path_ptr, int *argl, char ***args_ptr) {
                 ++p;
             arg_t = p;
         }
-        ++(*argl);
+        ++(*argl);  // Get a new argument
         char *arg = (char *)malloc((arg_t - arg_s + 1) * sizeof(char));
         strncpy(arg, arg_s, arg_t - arg_s);
         arg[arg_t - arg_s] = '\0';
         args[*argl - 1] = arg;
+        // Resize when the length is not enough
         if (*argl >= buf_size) {
             buf_size <<= 1;
             args = (char **)realloc(args, (buf_size) * sizeof(char *));
         }
     }
-    args[*argl] = NULL;
+    args[*argl] = NULL;  // Argument list should end with NULL
     *path_ptr = path;
     *args_ptr = args;
     return 0;
